b2a2498c7b7790bdfe9a77d69a02c9f6ab08c2ae
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-01-01 23:45:47 +0600

    first commit

---
+++ src/pages/IntroPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const IntroPage = () => {
+  return (
+    <div>
+      <h1>Welcome to Our Gift Shop</h1>
+      <p>We offer wide selection of gifts for all occasions.</p>
+      <button>Create Your Own Box</button>
+    </div>
+  );
+};
+
+export default IntroPage;
